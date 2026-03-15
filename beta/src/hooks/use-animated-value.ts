39b8f5ee00b47af2588b76f5054333b89f8b7b83
Michael Bilenko <129734190+mbilenko-florio@users.noreply.github.com>
2026-03-15 20:51:23 +0100

    add missing file

---
+++ beta/src/hooks/use-animated-value.ts
@@ -0,0 +1,12 @@
+import { useRef } from 'react';
+import { Animated } from 'react-native';
+
+/** useAnimatedValue polyfill that works with react-native-web */
+export function useAnimatedValue(initialValue: number): Animated.Value {
+  "use no memo";
+  const ref = useRef<Animated.Value | null>(null);
+  if (ref.current === null) {
+    ref.current = new Animated.Value(initialValue);
+  }
+  return ref.current;
+}
